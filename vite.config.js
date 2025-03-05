8c7516fa067bc8712d462a18f4c30ea4d2b777e8
redeslabs <archimedesfitapp@proton.me>
2025-03-05 23:13:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 /**
-* @type {import('vite').UserConfig}
-*/
+ * @type {import('vite').UserConfig}
+ */
 export default {
   // Set the base directory for GitHub pages
   base: process.env.NODE_ENV === 'production' ? '/gallery/' : '',  // Set to the repository name for GitHub Pages
@@ -10,14 +10,7 @@ export default {
     rollupOptions: {
       input: {
         main: 'index.html',
-        hallway: 'hallway.html',
-        // Include all JavaScript files as explicit entry points
-        mainJs: 'main.js',
-        hallJs: 'hall.js',
-        detailViewJs: 'detailView.js',
-        backgroundJs: 'background.js',
-        galleryDataJs: 'galleryData.js',
-        loadingJs: 'loading.js',
+        hallway: 'hallway.html'
       }
     }
   },
